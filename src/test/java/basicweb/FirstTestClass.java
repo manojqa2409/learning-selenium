95422f81bc81623f12a0dc4024f49b5b008f4e8b
manojqa2409 <manojqa2409@gmail.com>
2024-09-01 18:20:09 +0530

    Initial Commit

---
+++ src/test/java/basicweb/FirstTestClass.java
@@ -0,0 +1,52 @@
+package basicweb;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTestClass {
+	
+	private WebDriver driver;
+
+	@BeforeTest
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		//Use Chrome Driver
+		driver = new ChromeDriver();
+		//Maximize the Window
+		driver.manage().window().maximize();
+		// Timeout check
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+				
+	}
+	
+	@Test
+	public void testAmazonPage() {
+		//Open the Amazon URL
+		driver.get("https://www.amazon.in");
+		// Print the message
+		System.out.println("Browser Opened");
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+	
+		
+
+}
+
+
